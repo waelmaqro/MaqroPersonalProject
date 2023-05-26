6cd7d3364a1314274c52edf61766a96206cb7d12
Wael Hourani <wael.hourani@maqro.local>
2023-05-26 11:00:57 +1000

    adjusted tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = ({
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     "./node_modules/flowbite-react/**/*.js",
   ],
