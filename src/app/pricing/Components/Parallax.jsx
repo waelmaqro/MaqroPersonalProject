f8ddc7b9ae270183a62c3d689dc867c63ad7a0a0
Wael Hourani <wael.hourani@maqro.local>
2023-05-31 09:08:55 +1000

    commit removing parallax code

--- src/app/pricing/Components/Parallax.jsx
+++ src/app/pricing/Components/Parallax.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+// 'use client'
+// import React from 'react'
+// import "./styles.css";
+// import { useRef } from 'react';
+// import {
+//   motion,
+//   useScroll,
+//   useSpring,
+//   useTransform,
+//   MotionValue
+// } from "framer-motion";
+// import { motion } from 'framer-motion';
 
-const Parallax = () => {
-  return (
-    <div>parallax</div>
-  )
-}
+// const Parallax = () => {
+//   return (
+//     <div>parallax</div>
+//   )
+// }
 
-export default Parallax
+// export default Parallax
